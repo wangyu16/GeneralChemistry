1aad0d40cc08c6c1a5821533390807892c51ed79
Yu Wang <wangyu16@users.noreply.github.com>
2022-12-06 12:02:41 -0600

    Update streamlit_app_test.py

--- _drafts/streamlit_app_test.py
+++ _drafts/streamlit_app_test.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from sigfig import round
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
 
 st.title("Test")
 st.write(f"Round 3.1415926 in 3 significant figure gives {round(3.1415926,3)}")
@@ -9,4 +11,4 @@ df = pd.DataFrame({
   'second column': [10, 20, 30, 40]
 })
 
-df
+plt.plot(df['first column'],df['second column'])
